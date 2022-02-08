7fab3329bdd4abc7216a8c46a562f9e73301084d
andilorenzo88 <lorenzo.a.m1988@gmail.com>
2022-02-08 12:06:53 +0100

    crear app React

--- src/App.js
+++ src/App.js
@@ -4,20 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hola Coderhouse</h1>
+      <hr></hr>
+      <p>Desafío: Crear la App utilizando el CLI</p>
     </div>
   );
 }
